5d44abd30d0f828f445546442aefea1701db7bbb
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2024-05-28 18:55:44 +0530

    feat: addedd the meetups page and meetup details page.

--- meetups/apps.py
+++ meetups/apps.py
@@ -1,6 +1,8 @@
+"""Module providing a function printing python version."""
+
 from django.apps import AppConfig
 
 
 class MeetupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meetup'
+    name = 'meetups'
---
+++ meetups/urls.py
@@ -0,0 +1,11 @@
+"""Module providing a function printing python version."""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('meetups', views.index, name="all-meetups"),
+    path('meetups/<slug:meetup_slug>',
+         views.meetup_details, name="meetup-detail")
+]
